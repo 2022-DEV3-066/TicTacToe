147882352725a230fe0724b0ecaf871f786b677c
DaddyTeddy79 <tcoudiere@yahoo.fr>
2022-10-15 10:24:25 +0200

    Creating Service class TicTacToeGame and its testing class

---
+++ src/main/java/be/bnp/kata/tictactoe/services/TicTacToeGame.java
@@ -0,0 +1,8 @@
+package be.bnp.kata.tictactoe.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicTacToeGame {
+
+}
---
+++ src/test/java/be/bnp/kata/tictactoe/services/TicTacToeGameTest.java
@@ -0,0 +1,14 @@
+package be.bnp.kata.tictactoe.services;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class TicTacToeGameTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
