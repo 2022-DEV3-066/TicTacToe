679d5364da7c799810b2e5bb416d97dcd062f928
DaddyTeddy79 <tcoudiere@yahoo.fr>
2022-10-15 10:44:20 +0200

    TDD : launchNewGameShouldCreateNewBoard is GREEN
    Adding new class Board

---
+++ src/main/java/be/bnp/kata/tictactoe/model/Board.java
@@ -0,0 +1,5 @@
+package be.bnp.kata.tictactoe.model;
+
+public class Board {
+
+}
--- src/main/java/be/bnp/kata/tictactoe/services/TicTacToeGame.java
+++ src/main/java/be/bnp/kata/tictactoe/services/TicTacToeGame.java
@@ -2,7 +2,19 @@ package be.bnp.kata.tictactoe.services;
 
 import org.springframework.stereotype.Service;
 
+import be.bnp.kata.tictactoe.model.Board;
+
 @Service
 public class TicTacToeGame {
 
+	public void launchNewGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Board getBoard() {
+		// TODO Auto-generated method stub
+		return new Board();
+	}
+	
 }
