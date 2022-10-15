4c69a4180d4200466ff4ab6dee255022e8de4cd1
DaddyTeddy79 <tcoudiere@yahoo.fr>
2022-10-15 13:39:51 +0200

    TDD : threeOInFirstColumnShouldWinTheGame is RED

--- src/test/java/be/bnp/kata/tictactoe/services/TicTacToeGameTest.java
+++ src/test/java/be/bnp/kata/tictactoe/services/TicTacToeGameTest.java
@@ -102,4 +102,14 @@ class TicTacToeGameTest {
 		board.setSlotType(SlotType.X, 8);
 		assertEquals(SlotType.X, game.checkForAWinner());
 	}
+	
+	@Test
+	void threeOInFirstColumnShouldWinTheGame() {
+		game.launchNewGame();
+		Board board = game.getBoard();
+		board.setSlotType(SlotType.O, 0);
+		board.setSlotType(SlotType.O, 3);
+		board.setSlotType(SlotType.O, 6);
+		assertEquals(SlotType.O, game.checkForAWinner());
+	}
 }
