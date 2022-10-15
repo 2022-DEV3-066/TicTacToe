93cc1d06f38537bda03c09eac4c0073c192691df
DaddyTeddy79 <tcoudiere@yahoo.fr>
2022-10-15 13:00:01 +0200

    TDD : selectedSlotWithANonAvailableSlotTypeShouldThrowException is RED

--- src/test/java/be/bnp/kata/tictactoe/model/BoardTest.java
+++ src/test/java/be/bnp/kata/tictactoe/model/BoardTest.java
@@ -2,6 +2,7 @@ package be.bnp.kata.tictactoe.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -31,5 +32,12 @@ class BoardTest {
 	void settingSlotTypeToSlotNumberShouldUpdateSlotInBoardSlotList() {
 		board.setSlotType(SlotType.O, VALID_SLOT_NUMBER);
 		assertEquals(SlotType.O, board.getSlotList().get(VALID_SLOT_NUMBER).getSlotType());
+	}
+	
+	@Test
+	void selectedSlotWithANonAvailableSlotTypeShouldThrowException() {
+		Slot selectedSlot = board.getSlotList().get(VALID_SLOT_NUMBER);
+		selectedSlot.setSlotType(SlotType.O);
+		assertThrows(IllegalArgumentException.class, () -> board.setSlotType(SlotType.X, VALID_SLOT_NUMBER));
 	}
 }
