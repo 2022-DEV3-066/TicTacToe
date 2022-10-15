d2cb465ce3ca0a0f588d569e94492c8dffde761e
DaddyTeddy79 <tcoudiere@yahoo.fr>
2022-10-15 18:22:27 +0200

    TDD : initGameShouldDisplayIndexPage is RED

--- src/test/java/be/bnp/kata/tictactoe/controllers/TicTacToeControllerTest.java
+++ src/test/java/be/bnp/kata/tictactoe/controllers/TicTacToeControllerTest.java
@@ -1,14 +1,25 @@
 package be.bnp.kata.tictactoe.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+@WebMvcTest(TicTacToeController.class)
 class TicTacToeControllerTest {
 
+	@Autowired
+	MockMvc mockMvc;
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void initGameShouldDisplayIndexPage() throws Exception {
+		mockMvc.perform(get("/tictactoe"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("index"));
 	}
 
 }
