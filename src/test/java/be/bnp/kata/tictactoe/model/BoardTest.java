94540fe44d8a76adf3c3f9fe82928c9fe08c0e82
DaddyTeddy79 <tcoudiere@yahoo.fr>
2022-10-15 11:00:50 +0200

    TDD : newSlotListShouldHaveExactlyNineSlots in BoardTest is RED

--- src/test/java/be/bnp/kata/tictactoe/model/BoardTest.java
+++ src/test/java/be/bnp/kata/tictactoe/model/BoardTest.java
@@ -1,5 +1,6 @@
 package be.bnp.kata.tictactoe.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
@@ -9,5 +10,10 @@ class BoardTest {
 	@Test
 	void creatingNewBoardShouldCreateAListOfSlot() {
 		assertNotNull(new Board().getSlotList());
+	}
+	
+	@Test
+	void newSlotListShouldHaveExactlyNineSlots() {
+		assertEquals(9, new Board().getSlotList().size());
 	}
 }
