b685096ab816d38b8c8bca7a770eaba4ac98a501
DaddyTeddy79 <tcoudiere@yahoo.fr>
2022-10-15 18:23:39 +0200

    TDD : initGameShouldDisplayIndexPage is GREEN
    Adding initGame method in controller

--- src/main/java/be/bnp/kata/tictactoe/controllers/TicTacToeController.java
+++ src/main/java/be/bnp/kata/tictactoe/controllers/TicTacToeController.java
@@ -1,8 +1,13 @@
 package be.bnp.kata.tictactoe.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TicTacToeController {
 
+	@GetMapping("/tictactoe")
+	public String initGame() {
+		return "index";
+	}
 }
